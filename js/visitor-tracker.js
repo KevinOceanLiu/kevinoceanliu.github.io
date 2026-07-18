d1b7363b951845d92b383c185d9f8bcbe51507e7
KevinOceanLiu <yliuhaiyangocean@163.com>
2026-07-18 23:41:32 +0900

    Switch visitor analytics to GoatCounter

--- js/visitor-tracker.js
+++ js/visitor-tracker.js
@@ -1,26 +1,16 @@
 (function () {
     function loadVisitorTracker() {
-        if (document.getElementById('flagcounter')) {
+        if (document.getElementById('goatcounter')) {
             return;
         }
 
-        const container = document.createElement('div');
-        container.className = 'visitor-tracker';
-        container.setAttribute('aria-hidden', 'true');
+        const script = document.createElement('script');
+        script.id = 'goatcounter';
+        script.dataset.goatcounter = 'https://liuhaiyang.goatcounter.com/count';
+        script.async = true;
+        script.src = 'https://gc.zgo.at/count.js';
 
-        const link = document.createElement('a');
-        link.href = 'https://info.flagcounter.com/qfkW';
-        link.id = 'flagcounter';
-        link.tabIndex = -1;
-
-        const image = document.createElement('img');
-        image.src = 'https://s01.flagcounter.com/count2/qfkW/bg_FFFFFF/txt_000000/border_CCCCCC/columns_2/maxflags_10/viewers_0/labels_0/pageviews_0/flags_0/percent_0/';
-        image.alt = 'Flag Counter';
-        image.decoding = 'async';
-
-        link.appendChild(image);
-        container.appendChild(link);
-        document.body.appendChild(container);
+        document.body.appendChild(script);
     }
 
     if (document.readyState === 'loading') {
